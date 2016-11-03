0f8fe2e181aa96ad75b65a2ff616134399dad1e5
Ian Arsenault <ian_arsenault@protonmail.com>
2016-11-02 23:18:01 -0400

    Added 2 new problems

---
+++ squares-in-squares.py
@@ -0,0 +1,38 @@
+''' Write a program that will draw a square 5 times, multiplying in size each time'''
+
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+turt = turtle.Turtle()
+turt.color("red")
+
+
+def drawSquare(t, sz, n):
+    for i in range(4):
+        t.pendown()
+        t.forward(sz)
+        t.left(360 / n)
+
+def main():
+    size = 20
+    n = 4
+
+    posX = 0
+    posY = 0
+    penSize = 1
+
+    for i in range(10):
+        drawSquare(turt, size, n)
+        turt.pensize(penSize)
+        turt.penup()
+        posX = posX - 10
+        posY = posY - 10
+        turt.goto(posX, posY)
+        size = size + 20
+        penSize += 1
+
+    wn.exitonclick()
+
+if __name__ == '__main__':
+    main()
